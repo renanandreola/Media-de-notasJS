e1f2fcedbb5dadd7740c7eb2e721a6762251eca3
Aluno <renanandreolla@outlook.com>
2019-07-29 21:52:50 -0300

    Cálculo de média ponderada do aluno

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var a = parseFloat(prompt("Digite o valor da primeira nota"));
+var b = parseFloat(prompt("Digite o valor da segunda nota"));
+var c = parseFloat(prompt("Digite o valor da terceira nota"));
+var result = "";
+
+  result = a * 0.2 + b * 0.3 + c * 0.5
+
+document.body.append("Média =" + result)
